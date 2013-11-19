1bd767666f18b5506844540839b49ac349b0d577
subalary <aca10lfm@sheffield.ac.uk>
2013-11-19 11:39:02 +0000

    model working

--- IAF.m
+++ IAF.m
@@ -8,28 +8,34 @@ clear all
 dt = 0.01;
 t = 100;
 % membrane time constant
-tau = 10; %e-3
+tau = 10;
 % the spike threshold
-theta = -50; %e-3
-Vrest = -70; %e-3
-%  time window - maxsteps
+theta = -50;
+Vrest = -70;
+%  time window - max time
 T = round(t/dt);
 % voltage matrix
 Vmat = zeros(1, T);
 % refactory period
 ref = 0;
 %% MODEL
-Vmat(1,1) = Vrest;
+% initially set voltage to rest
+Vmat(1,1) = Vrest; 
 for step = 2:1:T
+	% Vnew = Vold(1-(dt/tau) + sum((Wij)/tau)*xj
+	% currently only updating linearly
 	Vmat(1, step)= Vmat(1, step-1)+(0.3/tau);
 	if Vmat(1, step) > theta
-		Vmat(1, step) = Vrest; % DECAY EXPONENTIALLY
+		% voltage reset - needs to decay exponentially
+		Vmat(1, step) = Vrest;
+		% apparently this makes the spikes look nice
 		Vmat(1, step-1) = 1;
 	end
 end
+% keep track of time for plotting
 time = 0:dt:(t-dt);
 %% PLOTTING
- figure(1);
- plot(time,Vmat)
- xlabel('Time (ms)');
- ylabel('Membrane potential (mV)');
+figure(1);
+plot(time,Vmat)
+xlabel('Time (ms)');
+ylabel('Membrane potential (mV)');
