8b43f5fcfcc1c4d9badc597e676f696fce5a5de7
subalary <aca10lfm@sheffield.ac.uk>
2013-11-19 11:18:59 +0000

    model working

--- IAF.m
+++ IAF.m
@@ -5,29 +5,29 @@
 clc
 clear all
 %% PARAMETER SETUP
-dt = 1;
+dt = 0.01;
 t = 100;
 % membrane time constant
 tau = 10; %e-3
 % the spike threshold
-theta = -55; %e-3
+theta = -50; %e-3
 Vrest = -70; %e-3
-Vreset = -80; %e-3
-%  time window
+%  time window - maxsteps
 T = round(t/dt);
 % voltage matrix
-Vmat = zeros(1, round(t/dt));
+Vmat = zeros(1, T);
 % refactory period
 ref = 0;
 %% MODEL
 Vmat(1,1) = Vrest;
 for step = 2:1:T
-	Vmat(1, step)= Vmat(1, step-1)+1;
+	Vmat(1, step)= Vmat(1, step-1)+(0.3/tau);
 	if Vmat(1, step) > theta
 		Vmat(1, step) = Vrest; % DECAY EXPONENTIALLY
+		Vmat(1, step-1) = 1;
 	end
 end
-time = 0:dt:T-dt;
+time = 0:dt:(t-dt);
 %% PLOTTING
  figure(1);
  plot(time,Vmat)
